65bee3789b269d61ff8111b0e1428894a1c29847
Elborn666 <108280320+Elborn666@users.noreply.github.com>
2023-06-10 18:28:36 +0300

    стилизация

--- src/page/MoviesDetails/MoviesDetails.jsx
+++ src/page/MoviesDetails/MoviesDetails.jsx
@@ -8,6 +8,7 @@ import {
 } from 'react-router-dom';
 import { getMovieById } from 'services/getMovies';
 import { BASE_POSTER_URL, PLACEHOLDER } from 'services/constants';
+import css from './MoviesDetails.module.css'
 
 
 const MoviesDetails = () => {
@@ -30,13 +31,13 @@ const MoviesDetails = () => {
 
   return (
     <>
-      <div className='css.back_link'>
+      <div className={css.back_link}>
         <Link to={backLinkHref}>
           Go back<span>.</span>
         </Link>
       </div>
-      <div className='css.container'>
-        <img className='css.img'
+      <div className={css.container}>
+        <img className={css.img}
           src={`${
             movie.poster_path
               ? BASE_POSTER_URL + movie.poster_path
@@ -45,27 +46,27 @@ const MoviesDetails = () => {
           alt="get"
         />
         <div>
-          <h2 className='css.title'>{movie.original_title}</h2>
-          <h3 className='css.rating'>Rating: {Math.round(movie.vote_average)}</h3>
-          <h4 className='css.overview'>Overview</h4>
-          <p className='css.description'>{movie.overview}</p>
-          <h5 className='css.genres'>Genres</h5>
-          <div className='css.desc_style'>
+          <h2 className={css.title}>{movie.original_title}</h2>
+          <h3 className={css.rating}>Rating: {Math.round(movie.vote_average)}</h3>
+          <h4 className={css.overview}>Overview</h4>
+          <p className={css.description}>{movie.overview}</p>
+          <h5 className={css.genres}>Genres</h5>
+          <div className={css.desc_style}>
             {movie.genres?.map(genre => (
               <li key={genre.id}>{genre.name}</li>
             ))}
           </div>
         </div>
       </div>
-      <div>
-        <h2>Additional information</h2>
-        <ul className='css.list'>
-          <li className='css.item'>
+      <div className={css.container}>
+        <h6 className={css.title}>Additional information</h6>
+        <ul className={css.list}>
+          <li className={css.item}>
             <NavLink to="cast" state={location.state}>
               Cast<span>.</span>
             </NavLink>
           </li>
-          <li className='css.item'>
+          <li className={css.item}>
             <NavLink to="reviews" state={location.state}>
               Reviews<span>.</span>
             </NavLink>
